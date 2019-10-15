10fcb6d322a67732edfca8967e27ac29c8c6760f
Theo-Sirius <52854615+Theo-Sirius@users.noreply.github.com>
2019-10-15 14:11:02 +0630

    Add files via upload

---
+++ nrf_receive_update.ino
@@ -0,0 +1,37 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(9, 10); // CE, CSN
+const byte address[6] = "00001";
+int LEDPin = A4;
+boolean switch1 = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LEDPin, OUTPUT);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+}
+void loop() {
+  if (radio.available()) {
+    char text[32] = "";
+    radio.read(&text, sizeof(text));
+    radio.read(&switch1, sizeof(switch1));
+
+    if (switch1 == 1) {
+      digitalWrite(LEDPin, HIGH);
+      Serial.println(text);
+
+    }
+
+    else {
+      digitalWrite(LEDPin, LOW);
+    }
+  }
+  else {
+    Serial.println("no");
+  }
+  delay(5);
+}
